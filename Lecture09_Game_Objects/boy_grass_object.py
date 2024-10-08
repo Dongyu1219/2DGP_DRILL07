dcb403ab895295c3762c73eeb7e8b154d4036c88
east_star00 <1219kdk@gmail.com>
2024-10-08 11:10:20 +0900

    2023180009제출

--- Lecture09_Game_Objects/boy_grass_object.py
+++ Lecture09_Game_Objects/boy_grass_object.py
@@ -1,5 +1,8 @@
+import random
 from pico2d import *
 from pygame.event import clear
+from pygame.examples.cursors import bitmap_cursor1
+
 
 # Game object class here
 class Grass:
@@ -15,6 +18,41 @@ class Grass:
     def draw(self):
         self.image.draw(400,30)
 
+class Boy:
+    def __init__(self):
+        self.x, self.y = random.randint(100, 700), 90
+        self.frame = random.randint(0, 7)
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
+    def draw(self):
+        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
+
+class BigBall:
+    def __init__(self):
+        self.x, self.y = random.randint(100, 700), 599
+        self.image = load_image('ball41x41.png')
+    def update(self):
+        self.y -= random.randint(1,10)
+        if self.y< 30:
+            self.y = 30
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+
+class SmallBall:
+    def __init__(self):
+        self.x, self.y = random.randint(100, 700), 599
+        self.image = load_image('ball21x21.png')
+    def update(self):
+        self.y -= random.randint(1,10)
+        if self.y< 30:
+            self.y = 30
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
 def handle_events():
     global running
     events = get_events()
@@ -27,18 +65,45 @@ def handle_events():
 def reset_world():
     global running
     global grass
+    global boy
+    global team
+    global smallteam
+    global bigteam
+    global world
+
     running =True
+    world=[]
+
     grass = Grass() #잔디를 생성한다(찍어낸다)
+    world.append(grass)
+    team = [Boy() for i in range(11)]
+    smallteam = [SmallBall() for i in range(10)]
+    bigteam = [BigBall() for i in range(10)]
 
 running = True
 
+
 def update_world():
-    grass.update() #객체의 상태를 업데이트, 시뮬레이션
+    for o in world : #객체의 상태를 업데이트, 시뮬레이션
+        o.update()
+    for boy in team:
+        boy.update()
+    for smallball in smallteam:
+        smallball.update()
+    for bigball in bigteam:
+        bigball.update()
     pass
 
 def render_world():
     clear_canvas()
-    grass.draw()
+    for o in world:
+        o.draw()
+    for boy in team:
+        boy.draw()
+    for smallball in smallteam:
+        smallball.draw()
+    for bigball in bigteam:
+        bigball.draw()
     update_canvas()
 
 open_canvas()
